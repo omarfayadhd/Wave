153c0bf43f6bf66174fd95535c649696ae0ef794
andthens <andanandans2002@gmail.com>
2021-12-01 13:13:33 +0530

    login

--- lib/main.dart
+++ lib/main.dart
@@ -27,36 +27,64 @@ class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: ListView(padding: EdgeInsets.symmetric(horizontal: 18.0),
-      children: <Widget>[
-        Column(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 18.0),
           children: <Widget>[
-        SizedBox(height: 80,),
-        Text("Login", style: TextStyle(fontSize: 25,))
-      ],
+            Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 80,
+                ),
+                Text("Login",
+                    style: TextStyle(
+                      fontSize: 25,
+                    ))
+              ],
+            ),
+            SizedBox(
+              height: 60,
+            ),
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                labelStyle: TextStyle(fontSize: 20),
+                filled: true,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  labelStyle: TextStyle(fontSize: 20),
+                  filled: true,
+                )),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: <Widget>[
+                ButtonTheme(
+                  height: 50,
+                  disabledColor: Colors.blueAccent,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text('Login',
+                        style: TextStyle(fontSize: 20, color: Colors.white)),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text("New User? Sign Up")
+              ],
+            )
+          ],
         ),
-        SizedBox(height: 60,),
-        TextField(
-          decoration: InputDecoration(
-            labelText: "Email",
-            labelStyle: TextStyle(fontSize: 20),
-            filled: true,
-          ) ,
-        ),
-        SizedBox(height: 20,),
-        TextField(
-          obscureText: true,
-          decoration: InputDecoration(
-            labelText: "Password",
-            labelStyle: TextStyle(fontSize: 20),
-            filled: true,
-          )
-        )
-
-      ],
-      ) 
       ),
-      );
-    
+    );
   }
 }
