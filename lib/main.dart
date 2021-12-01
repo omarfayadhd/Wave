adccbeb0be128c9c3926d40d457d25f818437d98
OmarFayadhd <canva4994@gmail.com>
2021-12-02 00:35:26 +0530

    buttons_added

--- lib/main.dart
+++ lib/main.dart
@@ -28,53 +28,92 @@ class LandingScreen extends StatelessWidget {
         backgroundColor: Colors.black12,
         //bottomNavigationBar: AppBar(),
         body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.home),
-                    color: Colors.white,
-                    iconSize: 30,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.play_arrow),
-                    iconSize: 30,
-                    color: Colors.blue,
-                    alignment: Alignment.bottomRight,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.celebration_rounded),
-                    color: Colors.red,
-                    iconSize: 30,
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.menu_open),
-                    color: Colors.yellow,
-                    iconSize: 30,
-                    alignment: Alignment.topRight,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.trending_up_sharp),
+                        color: Colors.white,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        iconSize: 30,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.search),
+                        iconSize: 30,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        color: Colors.blue,
+                        alignment: Alignment.bottomRight,
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.trending_up_outlined),
-                    color: Colors.white,
-                    iconSize: 30,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.home),
+                        color: Colors.white,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        iconSize: 30,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.play_arrow),
+                        iconSize: 30,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        color: Colors.blue,
+                        alignment: Alignment.bottomRight,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.celebration_rounded),
+                        color: Colors.red,
+                        iconSize: 30,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.menu_open),
+                        color: Colors.yellow,
+                        iconSize: 30,
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        alignment: Alignment.topRight,
+                      ),
+                    ],
                   ),
                 ],
-              )
-            ],
-          ),
-        ));
+              ),
+            ),
+          ],
+        )));
   }
 }
