65bedba7c7b9c1db0e2c2018660966771d5550d4
OmarFayadhd <canva4994@gmail.com>
2022-01-08 21:36:23 +0530

    logout

--- lib/app_screens/land.dart
+++ lib/app_screens/land.dart
@@ -1,10 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:wave_dev/app_screens/Profile.dart';
-//uihiuhuhuh
+import 'package:wave_dev/app_screens/login.dart';
+import 'package:wave_dev/model/user_model.dart';
 
-class land extends StatelessWidget {
+class land extends StatefulWidget {
   const land({Key? key}) : super(key: key);
+
+  @override
+  _landState createState() => _landState();
+}
+
+class _landState extends State<land> {
+  User? user = FirebaseAuth.instance.currentUser;
+  UserModel loggedInUser = UserModel();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseFirestore.instance
+        .collection("users")
+        .doc(user!.uid)
+        .get()
+        .then((value) {
+      this.loggedInUser = UserModel.fromMap(value.data());
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -193,17 +217,33 @@ class land extends StatelessWidget {
                               size: 25,
                               color: Colors.white,
                             ),
-                            onPressed: () {},
+                            onPressed: () {
+                              logout(context);
+                            },
                           ),
-                          const Text("Logout",
-                              style: TextStyle(
-                                  fontSize: 20.0,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white)),
+                          ActionChip(
+                            label: Text("Logout",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white)),
+                            onPressed: () {
+                              logout(context);
+                            },
+                          ),
                         ],
                       ),
                     ],
                   )));
         });
   }
+
+  profileView() {}
+}
+
+// the logout function
+Future<void> logout(BuildContext context) async {
+  await FirebaseAuth.instance.signOut();
+  Navigator.of(context)
+      .pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
 }
